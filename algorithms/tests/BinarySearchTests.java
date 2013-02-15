fcba58d5165b662bd0b8da631e93d9db922f8e70
mplacona <marcos.placona@gmail.com>
2013-02-15 17:36:21 +0000

    Added binary search

---
+++ algorithms/tests/BinarySearchTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import searchers.BinarySearch;
+
+public class BinarySearchTests {
+
+	public BinarySearchTests() {
+	}
+
+	@Test
+	public void testContains() {
+		// The array has already been sorted
+		int[]a = {1, 2, 3, 4, 7, 9, 28, 34, 82};
+		assertTrue(BinarySearch.contains(a, 1));
+		assertTrue(BinarySearch.contains(a, 4));
+		assertTrue(BinarySearch.contains(a, 7));
+		assertTrue(BinarySearch.contains(a, 82));
+	}
+
+}
