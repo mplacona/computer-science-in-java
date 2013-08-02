1b847fbcc5baa8391f874a9f3bdfa912962f1a90
Marcos Placona <marcos.placona@gmail.com>
2013-08-02 08:37:55 +0100

    removing old files

--- algorithms/sorters/ISort.java
+++
@@ -1,6 +0,0 @@
-package sorters;
-
-public interface ISort {
-	static final int arrNumbers[] =  {84,69,15,99,86,16,94,91};
-	public void sort();
-}
--- algorithms/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.util.Arrays;
-
-public class Utils {
-	public static void print(int arr[]) {
-		System.out.println(Arrays.toString(arr));
-	}
-}
